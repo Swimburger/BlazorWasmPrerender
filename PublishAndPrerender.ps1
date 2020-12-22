759e45214fd8b6d830f6870ac8230d4964c78c6b
Niels Swimberghe <nielsswimberghe@gmail.com>
2020-12-22 18:57:36 -0500

    strip out prerendered script tags as part of scripts

--- PublishAndPrerender.ps1
+++ PublishAndPrerender.ps1
@@ -7,8 +7,9 @@ dotnet publish .\Client\BlazorWasmPrerender.csproj -c Release -o Prerender/outpu
 Push-Location .\Prerender
 npx react-snap
 Get-ChildItem ".\output\wwwroot\*.html" -Recurse | ForEach-Object { 
-    (Get-Content -Path $_.FullName -Raw) `
-        -replace '<base href="/"','<base href="/BlazorWasmPrerender/"' | `
-        Set-Content -Path $_.FullName
+    $HtmlFileContent = (Get-Content -Path $_.FullName -Raw);
+    $HtmlFileContent = $HtmlFileContent.Replace('<base href="/"','<base href="/BlazorWasmPrerender/"')
+    $HtmlFileContent = $HtmlFileContent.Replace('<script type="text/javascript">var Module; window.__wasmmodulecallback__(); delete window.__wasmmodulecallback__;</script><script src="_framework/dotnet.5.0.1.js" defer="" integrity="sha256-SWZOE2EsCqc/7dPgJrcFqUvVvdeJ9cipeZ2NFMC9v2s=" crossorigin="anonymous"></script>','')
+    Set-Content -Path $_.FullName -Value $HtmlFileContent
 }
 Pop-Location
--- PublishAndPrerender.sh
+++ PublishAndPrerender.sh
@@ -5,5 +5,6 @@ pushd Prerender
 npx react-snap
 find . -name "*.html" | while read htmlFile; do
     sed -i 's/<base href="\/"/<base href="\/BlazorWasmPrerender\/"/g' $htmlFile
+    sed -i 's/<script type="text\/javascript">var Module; window.__wasmmodulecallback__(); delete window.__wasmmodulecallback__;<\/script><script src="_framework\/dotnet.5.0.1.js" defer="" integrity="sha256-SWZOE2EsCqc\/7dPgJrcFqUvVvdeJ9cipeZ2NFMC9v2s=" crossorigin="anonymous"><\/script>//g' $htmlFile
 done
 popd
